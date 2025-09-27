c8b70cfaa00ae90ac4894364996b2c085082ffef
nlhngan <nlhngan591@gmail.com>
2025-09-26 17:06:48 -0700

    Update engine.py

--- db/engine.py
+++ db/engine.py
@@ -4,6 +4,4 @@ from sqlalchemy.orm import declarative_base
 db_url="postgresql+psycopg://admin:admin@localhost:5432/pat"
 
 engine = create_engine(db_url)
-
 Base=declarative_base()
-Base.metadata.create_all(engine)
